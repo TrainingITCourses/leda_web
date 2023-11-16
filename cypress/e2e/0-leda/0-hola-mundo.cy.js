97de4d24e22bf5fa708ae1c69e13c252550a2030
Alberto Basalo <albertobasalo@hotmail.com>
2023-11-16 12:02:47 +0100

    test: hello cypress

---
+++ cypress/e2e/0-leda/0-hola-mundo.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('The Leda home page', () => { 
+
+  beforeEach(() => { 
+    cy.visit('http://127.0.0.1:8080'); 
+  });  
+
+  it('should be visitable', () => {
+  });
+  it('should have an h1', () => { 
+    cy.get('h1').should('contain', 'LedAir'); 
+  });
+  it('should have a link to identificacion', () => { 
+    cy.get('a').should('contain', 'Identificación');
+  });
+  it('should have a link to author page', () => { 
+    cy.get('#autor');
+  });
+});
