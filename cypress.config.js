2036b5c9e7066fa02cb8a8512071382aca3a233e
Alberto Basalo <albertobasalo@hotmail.com>
2023-11-17 09:23:36 +0100

    test: home page

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://127.0.0.1:8080",
+    browser: "chrome",
+    defaultCommandTimeout: 1000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/0-leda/0-hola-mundo.cy.js
+++ cypress/e2e/0-leda/0-hola-mundo.cy.js
@@ -3,18 +3,24 @@
 describe('The Leda home page', () => { 
 
   beforeEach(() => { 
-    cy.visit('http://127.0.0.1:8080'); 
+    cy.visit(''); 
   });  
 
   it('should be visitable', () => {
   });
   it('should have an h1', () => { 
-    cy.get('h1').should('contain', 'LedAir'); 
+    cy.get('h1').should('contain', "LedAir");
   });
   it('should have a link to identificacion', () => { 
     cy.get('a').should('contain', 'Identificación');
   });
   it('should have a link to author page', () => { 
-    cy.get('#autor');
+    cy.get('a.enlace-externo')
+      .should('contain', "Alberto")
+      .and('have.attr','href','https://www.albertobasalo.dev/');
   });
+  it("should navigate to login page", () => {
+    cy.get('header > a').click();
+    cy.url().should('include', 'login');
+  })
 });
